a2afd143e9f2e5bffe208f7a55e8e5c2c0d09593
sagar-pardhi <sgrpardhi007@gmail.com>
2024-11-30 20:22:07 +0530

    add: request validation

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -7,12 +7,14 @@ import (
 	"log/slog"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/sagar-pardhi/students-api/internal/types"
 	"github.com/sagar-pardhi/students-api/internal/utils/response"
 )
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("creating a student")
 		var student types.Student
 
 		err := json.NewDecoder(r.Body).Decode(&student)
@@ -22,7 +24,18 @@ func New() http.HandlerFunc {
 			return
 		}
 
-		slog.Info("creating a student")
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+
+		// validate request
+		if err := validator.New().Struct(student); err != nil {
+			validateErrs := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrs))
+			return
+		}
+
 		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "ok"})
 	}
 }
