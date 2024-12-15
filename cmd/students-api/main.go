82801ab50f8ce84f06d005b9514e74d1f9410b13
sagar-pardhi <sgrpardhi007@gmail.com>
2024-12-15 12:55:17 +0530

    feat: add students list

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -31,6 +31,7 @@ func main() {
 
 	router.HandleFunc("POST /api/students", student.New(storage))
 	router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
+	router.HandleFunc("GET /api/students", student.GetList(storage))
 
 	server := http.Server{
 		Addr:    cfg.Address,
--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -80,3 +80,18 @@ func GetById(storage storage.Storage) http.HandlerFunc {
 	}
 
 }
+
+func GetList(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("getting all students")
+
+		students, err := storage.GetStudents()
+
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, err)
+		}
+
+		response.WriteJson(w, http.StatusOK, students)
+	}
+
+}
